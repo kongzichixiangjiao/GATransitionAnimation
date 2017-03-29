b6357b67a791e09ed1c9aee8d0059eadf6d18130
kongzichixiangjiao <58895347@qq.com>
2017-03-29 18:12:39 +0800

    LikeButton 组合动画实现的点赞按钮

---
+++ tool/KeyPath.swift
@@ -0,0 +1,17 @@
+//
+//  KeyPath.swift
+//  GATransitionAnimation
+//
+//  Created by houjianan on 2017/3/29.
+//  Copyright © 2017年 houjianan. All rights reserved.
+//
+
+import Foundation
+
+
+enum KeyPath: String {
+    case scale = "transform.scale",
+         path = "path",
+         position = "position",
+         alpha = "opacity"
+}
